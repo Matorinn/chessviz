be88794a93a98af66c6a35b823c6f495ea123cbb
Mato <dany.lesckovetz@gmail.com>
2019-04-15 05:11:52 +0700

    Fix of error from travic

--- src/board_print_plain.cpp
+++ src/board_print_plain.cpp
@@ -2,14 +2,15 @@
 
 using namespace std;
 
-void draw(char A[8][8]) {
-  int i, j;
-  for (i = 7; i > -1; i--) {
-    cout << i + 1 << " ";
-    for (j = 0; j < 8; j++) {
-      cout << A[i][j] << " ";
+void draw(char A[8][8])
+{
+    int i, j;
+    for (i = 7; i > -1; i--) {
+        cout << i + 1 << " ";
+        for (j = 0; j < 8; j++) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
-  cout << "  a b c d e f g h" << endl;
+    cout << "  a b c d e f g h" << endl;
 }
