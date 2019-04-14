05f03379620158bb8e5b3ce6e57615bd2933ad18
Mato <dany.lesckovetz@gmail.com>
2019-04-15 04:57:40 +0700

    Add some of pawn steps

--- Makefile
+++ Makefile
@@ -1,2 +1,13 @@
-all :
-	g++ -Wall src/main.cpp src/board_print_plain.cpp -o chessviz
+.PHONY: clean all
+all: bin/chessviz
+bin/chessviz: build/main.o build/board_print_plain.o
+	g++ $^ -o $@
+
+build/main.o: src/main.cpp
+	g++ -c -Wall -Werror -o $@ $<
+
+build/board_print_plain.o: src/board_print_plain.cpp
+	g++ -c -Wall -Werror -o $@ $<
+
+clean:
+	rm build/*.o	
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,56 @@
 #include "board_print_plain.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-  char A[8][8] = {{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-                  {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                  {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                  {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}};
-  draw(A);
-  return 0;
+int main()
+{
+    char a[8][8] = {{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+                    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}};
+    draw(a);
+    string move;
+    char temp;
+    getline(cin, move);
+    cout << "1 element =" << move[1] - '1' << "/" << move[0] - 'a' << endl;
+    temp = a[move[1] - '1'][move[0] - 'a'];
+    if (move[2] == '-') {
+        if (a[move[4] - '1'][move[0] - 'a'] != ' '
+            || (a[move[1] - '1'][move[0] - 'a'] != 'p'
+                && a[move[1] - '1'][move[0] - 'a'] != 'P')
+            || (move[0] != move[3])
+            || (abs((int)(move[1] - '1') - (int)(move[4] - '1'))) != 1)
+        /*1 срубание в простой ход? 2 пешка ли? 3 ровность хода? 4 дальность
+           хода*/
+        {
+            cout << "ERROR WRONG INPUT";
+            return 0;
+        } else {
+            a[move[1] - '1'][move[0] - 'a'] = ' ';
+            a[move[4] - '1'][move[3] - 'a'] = temp;
+        }
+    } else if (move[2] == 'x') {
+        if (a[move[4] - '1'][move[3] - 'a'] == ' '
+            || (a[move[1] - '1'][move[0] - 'a'] != 'p'
+                && a[move[1] - '1'][move[0] - 'a'] != 'P')) {
+            cout << "ERROR WRONG INPUT";
+            return 0;
+        } else {
+            a[move[1] - '1'][move[0] - 'a'] = ' ';
+            a[move[4] - '1'][move[3] - 'a'] = temp;
+        }
+    } else {
+        cout << "ERROR WRONG INPUT";
+        return 0;
+    }
+    cout << "wow" << temp << "wow" << endl;
+    draw(a);
+    return 0;
 }
