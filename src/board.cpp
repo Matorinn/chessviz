5ca43c8162bfb3fd5d81fcbd08a5254cf20eb95b
Mato <dany.lesckovetz@gmail.com>
2019-05-13 05:56:08 +0700

    Add move of rook

--- src/board.cpp
+++ src/board.cpp
@@ -38,3 +38,37 @@ int king(string move, char (&a)[8][8])
     draw(a);
     return 0;
 }
+
+int rook(string move, char (&a)[8][8])
+{
+    char temp;
+    temp = a[move[2] - '1'][move[1] - 'a'];
+    if (move[3] == '-') {
+        if ((a[move[2] - '1'][move[1] - 'a'] != 'R'
+             && a[move[2] - '1'][move[1] - 'a'] != 'r')
+            || a[move[5] - '1'][move[4] - 'a'] != ' '
+            || ((abs((int)(move[2] - '1') - (int)(move[5] - '1')) > 0)
+                && (abs((int)(move[1] - 'a') - (int)(move[4] - 'a')) > 0))) {
+            cout << "WRONG INPUT\n";
+            return 1;
+        } else {
+            a[move[2] - '1'][move[1] - 'a'] = ' ';
+            a[move[5] - '1'][move[4] - 'a'] = temp;
+        }
+    }
+    if (move[3] == 'x') {
+        if ((a[move[2] - '1'][move[1] - 'a'] != 'R'
+             && a[move[2] - '1'][move[1] - 'a'] != 'r')
+            || a[move[5] - '1'][move[4] - 'a'] == ' '
+            || ((abs((int)(move[2] - '1') - (int)(move[5] - '1')) > 0)
+                && (abs((int)(move[1] - 'a') - (int)(move[4] - 'a')) > 0))) {
+            cout << "WRONG INPUT\n";
+            return 1;
+        } else {
+            a[move[2] - '1'][move[1] - 'a'] = ' ';
+            a[move[5] - '1'][move[4] - 'a'] = temp;
+        }
+    }
+    draw(a);
+    return 0;
+}
